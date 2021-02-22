28d173e8e3510a5f91a89235baa60fa428e3fc0e
viniciusprog777 <viniciusjetmax@gmail.com>
2021-02-22 16:45:06 -0300

    Alterando a porta do Server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const app = require("./app")
+const app = require("./app");
 //porta usada pelo servidor
-const port = 3333
+const port = process.env.PORT || 3333;
 
-//subindo o servidor na web 
+//subindo o servidor na web
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-})
+  console.log(`Servidor rodando na porta ${port}`);
+});
